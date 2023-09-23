87f0c9fd97b27da2ffbcd9b089eb0ad02d0858be
mohammad-alghzawi <mohd.a.ghzawi@gmail.com>
2023-09-23 14:03:45 +0300

    mohammad

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.index');
+    return view('pages.shop');
 });
 
 Route::get('/dashboard', function () {
